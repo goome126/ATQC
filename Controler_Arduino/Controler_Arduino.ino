ad54911ff7d3414f88bd81113df18e241b74e3ca
jarom77 <17jchristensen5@go.dsdmail.net>
2017-01-30 14:33:04 -0700

    Made it work
    + Changed baud rate to 250000
    + Read commands with a function that loops until read int > 0
    + Print out what changed and what it is

--- Controler_Arduino/Controler_Arduino.ino
+++ Controler_Arduino/Controler_Arduino.ino
@@ -1,28 +1,51 @@
 int pitch=0,roll=0,yah=0,altitude=0,command;
 void setup() {
   //short command;  //pitch 1, roll 2, yah 3, altitude 4
-  Serial.begin(57600);
+  Serial.begin(250000);
   while (!Serial) { }
   Serial.println("Arduino Online");
 }
 
 void loop() {
-    command = Serial.parseInt();
+    
+    command = nextInt();
+    Serial.println(command);
 
-  if(command == 1)
-    pitch = Serial.parseInt();
+  if(command == 1) {
+    pitch = nextInt();
+    Serial.println("pitch=");
+    Serial.println(pitch);
+  }
 
-  if(command == 2)
-    roll = Serial.parseInt();
+  if(command == 2) {
+    roll = nextInt();
+    Serial.println("roll=");
+    Serial.println(roll);
+  }
 
-  if(command == 3)
-    yah = Serial.parseInt();
+  if(command == 3) {
+    yah = nextInt();
+    Serial.println("yah=");
+    Serial.println(yah);
+  }
 
-  if(command == 4)
-    altitude = Serial.parseInt();
+  if(command == 4) {
+    altitude = nextInt();
+    Serial.println("altitude=");
+    Serial.println(altitude);
+  }
 
   analogWrite(11, pitch); 
   analogWrite(10, roll);
   analogWrite(9, yah);
   analogWrite(6, altitude);
 }
+
+int nextInt() {
+  int input = NULL;
+  while (input == NULL) {
+    input = Serial.parseInt();
+  }
+  return input;
+}
+
