337dc4e954752133b7912be01d282e3554f88a6b
goome126 <goome126@live.com>
2017-02-01 14:36:23 -0700

    New Controller File That Works
    Created a compeltely new controller file. This one is very similar to
    the old controller, but it uses Serial.Read instead of Serial.ParseInt.
    In order to make Serial.Read work a delay of 5 microseconds is required
    at the beginning of each case statement to avoid getting the console
    spammed with -1.  Serial.Read will only read one byte at a time, thus
    the need to do math to get numbers in the right position.

---
+++ Controler_Arduino/nathan_controller/nathan_controller.ino
@@ -0,0 +1,87 @@
+int command = 0;
+int pitch,altitude,yaw,roll = 0;
+char pos1,pos2,pos3= '0';
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Controller Running");
+}
+
+void loop() {
+  command = Serial.read();
+
+//if (command > 0){
+
+  switch (command) {
+    case '1':
+    delay(5);
+    pos1 = Serial.read();
+    Serial.println(pos1);
+    pitch = ((int)pos1 - 48) * 100;
+    pos2 = Serial.read();
+    Serial.println(pos2);
+    pitch = pitch + ((int)pos2 - 48) * 10;
+    pos3 = Serial.read();
+    Serial.println(pos3);
+    pitch = pitch + ((int)pos3 - 48);
+    Serial.print("pitch = ");
+    Serial.println(pitch);
+    break;
+
+    case '2':
+    delay(5);
+    pos1 = Serial.read();
+    Serial.println(pos1);
+    roll = ((int)pos1 - 48) * 100;
+    pos2 = Serial.read();
+    Serial.println(pos2);
+    roll = roll + ((int)pos2 - 48) * 10;
+    pos3 = Serial.read();
+    Serial.println(pos3);
+    roll = roll + ((int)pos3 - 48);
+    Serial.print("roll = ");
+    Serial.println(roll);
+    break;
+
+    case '3':
+    delay(5);
+    pos1 = Serial.read();
+    Serial.println(pos1);
+    yaw = ((int)pos1 - 48) * 100;
+    pos2 = Serial.read();
+    Serial.println(pos2);
+    yaw = yaw + ((int)pos2 - 48) * 10;
+    pos3 = Serial.read();
+    Serial.println(pos3);
+    yaw = yaw + ((int)pos3 - 48);
+    Serial.print("yaw = ");
+    Serial.println(yaw);
+    break;
+    
+    case '4':
+    delay(5);
+    pos1 = Serial.read();
+    Serial.println(pos1);
+    altitude = ((int)pos1 - 48) * 100;
+    pos2 = Serial.read();
+    Serial.println(pos2);
+    altitude = altitude + ((int)pos2 - 48) * 10;
+    pos3 = Serial.read();
+    Serial.println(pos3);
+    altitude = altitude + ((int)pos3 - 48);
+    Serial.print("altitude = ");
+    Serial.println(altitude);
+    break;
+
+    default:
+      //Serial.println("No direction selected");
+    break;
+    }
+//}
+    Serial.flush();
+    analogWrite(11,pitch);
+    analogWrite(10,roll);
+    analogWrite(9,yaw);
+    analogWrite(6,altitude);
+  
+}
