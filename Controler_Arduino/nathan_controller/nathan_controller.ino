bc0679369ea22f9a3cde40f8d8834fe65f7fa4f4
jarom77 <17jchristensen5@go.dsdmail.net>
2017-02-09 14:06:05 -0700

    One-byte control
    Changed the code to allow values to be changed with a single byte from
    Serial. See comments before loop for details. You can also set all
    values at once by sending 4 characters.

--- Controler_Arduino/nathan_controller/nathan_controller.ino
+++ Controler_Arduino/nathan_controller/nathan_controller.ino
@@ -1,50 +1,53 @@
-int pitch,altitude,yaw,roll,command,value;
+int pitch,altitude,yaw,roll;
 
 void setup() {
   Serial.begin(9600); //Initalize serial
   Serial.println("Controller Running"); //Make sure serial is initialized
-  Serial.setTimeout(5); //Reduce delay on readstring
 }
+
+// this method reads a command and value as one byte, using the first 2 bits to determine the
+// command and the last 6 to determine the value to set. Because 6 bits can only hold numbers
+// between 0 and 63, we multiply by 2 to get numbers between 0 and 126. So only even numbers can
+// be used as values to set.
 
 void loop() {
-String cline = Serial.readString(); //Read a string out of serial and assign that to cline
-char commandChar = cline.charAt(0); //Take the first character out of the string to determine what you're controlling
-command = (int)commandChar - 48; //Convert that character to an int
-value = cline.substring(1).toInt(); //Store the actual value you're trying to pass into a value
+  while (Serial.available() < 1) {} //wait until there is at least one byte comming from Serial
+  int input = Serial.read(); //Read one byte of Serial
+  int command = input/64; //get front two bits
+  int value = 2*(input%64); //get last 6 bits with modular division
 
-switch (command) { //Have a switch statement that determines what you're controlling
-case 1:
- pitch = value;
- Serial.print("pitch = ");
- Serial.println(pitch);
- break;
-    
-case 2:
- roll = value; 
- Serial.print("roll = ");
- Serial.println(roll);
- break;
-
-case 3:
- yaw = value; 
- Serial.print("yaw = ");
- Serial.println(yaw);
- break;
+  switch (command) { //Have a switch statement that determines what you're controlling
+  case 1:
+  pitch = value;
+  Serial.print("pitch = ");
+  Serial.println(pitch);
+  break;
+  
+  case 2:
+   roll = value; 
+   Serial.print("roll = ");
+   Serial.println(roll);
+   break;
+  
+  case 3:
+   yaw = value; 
+   Serial.print("yaw = ");
+   Serial.println(yaw);
+   break;
+     
+  case 0:
+   altitude = value; 
+   Serial.print("altitude = ");
+   Serial.println(altitude);
+   break;
+  
+   default:
    
-case 4:
- altitude = value; 
- Serial.print("altitude = ");
- Serial.println(altitude);
- break;
-
- default:
- 
- break;
-}
+   break;
+  }
     Serial.flush();
     analogWrite(11,pitch);
     analogWrite(10,roll);
     analogWrite(9,yaw);
     analogWrite(6,altitude);
-  
 }
