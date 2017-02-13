586c85377ae3ffe4f8a105ca66db455dec7ded4a
Dylan <dylan@splapfish.com>
2017-02-13 13:29:14 -0700

    Removed old tryle code
    Replaced the only drive code with the newer one

--- Controler_Arduino/Controler_Arduino.ino
+++ Controler_Arduino/Controler_Arduino.ino
@@ -1,61 +1,53 @@
-int pitch=0,roll=0,yah=0,altitude=0;
-short command;
+int pitch,altitude,yaw,roll;
+
 void setup() {
-  Serial.begin(250000);
-  while (!Serial) { }
-  Serial.println("Arduino Online");
+  Serial.begin(9600); //Initalize serial
+  Serial.println("Controller Running"); //Make sure serial is initialized
 }
+
+// this method reads a command and value as one byte, using the first 2 bits to determine the
+// command and the last 6 to determine the value to set. Because 6 bits can only hold numbers
+// between 0 and 63, we multiply by 2 to get numbers between 0 and 126. So only even numbers can
+// be used as values to set.
 
 void loop() {
-    
-    command = nextInt();   //pitch 1, roll 2, yah 3, altitude 4
-    Serial.println(command);
-switch (command) {
+  while (Serial.available() < 1) {} //wait until there is at least one byte comming from Serial
+  int input = Serial.read(); //Read one byte of Serial
+  int command = input/64; //get front two bits
+  int value = 2*(input%64); //get last 6 bits with modular division
+
+  switch (command) { //Have a switch statement that determines what you're controlling
   case 1:
-    pitch = nextInt();
-    Serial.println("pitch=");
-    Serial.println(pitch);
+  pitch = value;
+  Serial.print("pitch = ");
+  Serial.println(pitch);
   break;
-
+  
   case 2:
-    roll = nextInt();
-    Serial.println("roll=");
-    Serial.println(roll);
-  break;
-
+   roll = value; 
+   Serial.print("roll = ");
+   Serial.println(roll);
+   break;
+  
   case 3:
-    yah = nextInt();
-    Serial.println("yah=");
-    Serial.println(yah);
-  break;
-
-  case 4:
-    altitude = nextInt();
-    Serial.println("altitude=");
-    Serial.println(altitude);
-  break;
-}
-  analogWrite(11, pitch); 
-  analogWrite(10, roll);
-  analogWrite(9, yah);
-  analogWrite(6, altitude);
-}
-
-int nextInt() {
-  int input = NULL;
-  while (input == NULL) {
-    input = Serial.parseInt();
-    if(Serial.available()==false){//if losses connition to computer will drop out of sky
-      analogWrite(11, 0); 
-      analogWrite(10, 0);
-      analogWrite(9, 0);
-      while(altitude != 0){
-        --altitude;
-        analogWrite(6, altitude);
-      }
-    }
-      return input;
+   yaw = value; 
+   Serial.print("yaw = ");
+   Serial.println(yaw);
+   break;
+     
+  case 0:
+   altitude = value; 
+   Serial.print("altitude = ");
+   Serial.println(altitude);
+   break;
+  
+   default:
+   
+   break;
   }
+    Serial.flush();
+    analogWrite(11,pitch);
+    analogWrite(10,roll);
+    analogWrite(9,yaw);
+    analogWrite(6,altitude);
 }
-
-
