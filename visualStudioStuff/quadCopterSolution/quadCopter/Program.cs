de4627a1772c7661973f3c32ec7f1ce7f479842b
goome126 <goome126@live.com>
2017-05-26 14:13:08 -0600

    Updated C# Code
    Project solution for visual studio is now included in github

--- visualStudioStuff/quadCopterSolution/quadCopter/Program.cs
+++ visualStudioStuff/quadCopterSolution/quadCopter/Program.cs
@@ -14,7 +14,10 @@ namespace arduinoController
         {
             String command = "h";
             String number = "";
-            int numMan = 0;
+            int altitudePower = 0;
+            int yawPower = 0;
+            int rollPower = 0; 
+            int pitchPower = 0;
             bool loopManager = true;
 
             //Instantiate the communications
@@ -24,51 +27,65 @@ namespace arduinoController
 
             //Open the port for communication
             port.Open();
-            while (loopManager == true) {
+            while (loopManager == true)
+            {
 
                 var kbInput = Console.ReadKey();
-                switch (kbInput.Key) {
+                switch (kbInput.Key)
+                {
                     case ConsoleKey.W:
-                        command = "4";
-                        numMan += 10;
-                        number = command + numMan.ToString();
+                        command = "3";
+                        yawPower += 10;
+                        number = command + yawPower.ToString();
                         Console.WriteLine("w");
-                    break;
+                        break;
 
                     case ConsoleKey.S:
-                        command = "4";
-                        numMan -= 10;
-                        number = command + numMan.ToString();
+                        command = "3";
+                        yawPower -= 10;
+                        number = command + yawPower.ToString();
                         Console.WriteLine("s");
-                    break;
+                        break;
                     case ConsoleKey.UpArrow:
-                        command = "1";
-                        numMan += 10;
-                        number = command + numMan.ToString();
+                        command = "4";
+                        altitudePower += 10;
+                        number = command + altitudePower.ToString();
                         break;
                     case ConsoleKey.DownArrow:
-                        command = "1";
-                        numMan += 10;
-                        number = command + numMan.ToString();
+                        command = "4";
+                        altitudePower += 10;
+                        number = command + altitudePower.ToString();
                         break;
                     case ConsoleKey.LeftArrow:
-                        command = "2";
-                        numMan -= 10;
-                        number = command + numMan.ToString();
+                        command = "1";
+                        pitchPower -= 10;
+                        number = command + pitchPower.ToString();
                         break;
                     case ConsoleKey.RightArrow:
-                        command = "2";
-                        numMan += 10;
-                        number = command + numMan.ToString();
+                        command = "1";
+                        pitchPower += 10;
+                        number = command + pitchPower.ToString();
                         break;
                     case ConsoleKey.Spacebar:
+                        number = "40";
+                        //port.Write(number);
+                        /*number = "30";
+                        port.Write(number);
+                        number = "20";
+                        port.Write(number);
                         number = "10";
+                        port.Write(number);*/
+                        altitudePower = 0;
+                        pitchPower = 0;
+                        yawPower = 0;
+                        rollPower = 0;
                         Console.WriteLine("Kill");
                         break;
                 }
 
 
                 port.Write(number);
+                Console.WriteLine(number);
 
             }
             port.Close();
